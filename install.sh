03d61e1f36d12cc4498fd6f4f483a686e0634826
Craig Zeise <czeise@medicomhealth.com>
2022-01-24 22:24:41 -0600

    initial commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Installs dotfiles using the configuration in rcrc. For work dotfiles, add the
+# -d crx-dotfiles flag. See the RCM instructions for details.
+# https://thoughtbot.github.io/rcm/
+RCRC=rcrc rcup
